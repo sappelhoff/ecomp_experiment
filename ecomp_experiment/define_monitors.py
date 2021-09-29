5a5f8f1537997599b1462a35887c61f73a7585cf
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2021-09-29 10:47:56 +0200

    add new monitor

--- ecomp_experiment/define_monitors.py
+++ ecomp_experiment/define_monitors.py
@@ -8,17 +8,24 @@ if __name__ == "__main__":
     # Define new monitors
     # -------------------
 
-    # My Dell Latitude 7490 monitor
+    # Dell Latitude 7490 monitor (personal laptop)
     my_monitor = monitors.Monitor(name="latitude7490")
     my_monitor.setSizePix((1920, 1080))
     my_monitor.setWidth(30)
     my_monitor.setDistance(50)
     my_monitor.saveMon()
 
-    # Eizo Foris monitor in the lab
+    # Eizo Foris monitor in the EEG lab (stimulus PC)
     my_monitor = monitors.Monitor(name="eizoforis")
     my_monitor.setSizePix((2560, 1440))
     my_monitor.setWidth(60)
+    my_monitor.setDistance(50)
+    my_monitor.saveMon()
+
+    # BENQ EX2780Q monitor (personal desktop)
+    my_monitor = monitors.Monitor(name="latitude7490")
+    my_monitor.setSizePix((2560, 1440))
+    my_monitor.setWidth(59.5)
     my_monitor.setDistance(50)
     my_monitor.saveMon()
 
