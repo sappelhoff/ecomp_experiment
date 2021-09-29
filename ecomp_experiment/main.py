7a6f6255cc9a3c24d89df8d3aacf86d952dae066
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2021-09-29 14:23:22 +0200

    continue prototyping

--- ecomp_experiment/main.py
+++ ecomp_experiment/main.py
@@ -3,23 +3,20 @@
 # %%
 
 import numpy as np
-from psychopy import core, monitors, visual
+from psychopy import monitors, visual
 
 from ecomp_experiment.define_settings import EXPECTED_FPS
-from ecomp_experiment.define_stimuli import get_fixation_stim
-from ecomp_experiment.utils import set_fixstim_color
+from ecomp_experiment.define_stimuli import get_digit_stims, get_fixation_stim
+from ecomp_experiment.define_trials import gen_trials
+from ecomp_experiment.utils import check_framerate
 
 # %%
-win = visual.Window()
 
-# %%
-win.close()
-# %%
 
+trials = gen_trials(5)
 
 my_monitor = monitors.Monitor(name="benq")
 width, height = my_monitor.getSizePix()
-# %%
 
 
 win = visual.Window(
@@ -31,44 +28,42 @@ win = visual.Window(
     size=(width, height),
 )
 
-fps = win.getActualFrameRate(nMaxFrames=1000)
+fps = check_framerate(win, EXPECTED_FPS)
 
-# On which frame rate are we operating? Try getting it several times
-# because it can fluctuate a bit
-fps_counter = 0
-while True:
-    fps = win.getActualFrameRate(nMaxFrames=1000)
-    if fps is not None:
-        fps = int(round(fps))
-        print("found fps: {}".format(fps))
-    if EXPECTED_FPS == fps:
-        break
-    else:
-        fps_counter += 1
-        core.wait(1)
-    if fps_counter > 3:
-        raise ValueError(
-            "Please adjust the EXPECTED_FPS variable " "in define_settings.py"
-        )
 
+def display_trial(win, trial, digit_frames, blank_frames, digit_stims):
+    """Display a trial on a window."""
+    for digit in trial:
+
+        # Draw digit
+        digit_stims[digit].draw()
+        for frame in range(digit_frames):
+            win.flip()
+
+        # Blank screen before next digit
+        for frame in range(blank_frames):
+            win.flip()
+
+
+# get digits
+digit_stims = get_digit_stims(win, height=5)
 
 # Get the objects for the fixation stim
 outer, inner, horz, vert = get_fixation_stim(win)
 fixation_stim_parts = [outer, horz, vert, inner]
 
 
-for stim in fixation_stim_parts:
-    stim.setAutoDraw(True)
+trial = trials[2, ...]
+for trial in trials:
+    display_trial(win, trial, int(fps / 3), int(fps / 12), digit_stims)
+    for frame in range(fps):
+        win.flip()
 
-for frame in range(fps * 3):
-    win.flip()
-    if frame % 10 == 0:
-        set_fixstim_color(
-            inner, [(1, 0, 0), (0, 1, 0), (0, 0, 1)][np.random.choice([0, 1, 2])]
-        )
-
+    print(np.abs(trial).mean())
 
 win.close()
+
+
 # %%
 win.close()
 # %%
--- ecomp_experiment/utils.py
+++ ecomp_experiment/utils.py
@@ -1,8 +1,27 @@
 """Provide utility functions for the main experiment."""
 
+from psychopy import core
+
 
 def set_fixstim_color(stim, color):
     """Set the fill and line color of a stim."""
     stim.setFillColor(color)
     stim.setLineColor(color)
     return stim
+
+
+def check_framerate(win, expected_fps):
+    """Get and check fps of this window."""
+    fps_counter = 0
+    while True:
+        fps = win.getActualFrameRate(nMaxFrames=1000)
+        if fps is not None:
+            fps = int(round(fps))
+        if expected_fps == fps:
+            return fps
+        else:
+            fps_counter += 1
+            print(f"Found fps: {fps}, trying again.")
+            core.wait(0.5)
+        if fps_counter > 3:
+            raise ValueError(f"Are you sure you are expecting {expected_fps} fps?")
