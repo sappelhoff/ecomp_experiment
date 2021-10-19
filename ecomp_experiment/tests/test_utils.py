cec6d7c9cd2538500a53fb043c7740ef56bf47fc
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2021-10-19 11:51:14 +0200

    add test for savedict

--- ecomp_experiment/tests/test_utils.py
+++ ecomp_experiment/tests/test_utils.py
@@ -1,10 +1,12 @@
 """Test utility functions."""
 import itertools
 
+import numpy as np
+import pandas as pd
 import pytest
 
 from ecomp_experiment.define_settings import KEYLIST_DICT
-from ecomp_experiment.utils import map_key_to_choice
+from ecomp_experiment.utils import map_key_to_choice, save_dict
 
 
 @pytest.mark.parametrize(
@@ -38,3 +40,17 @@ def test_map_key_to_choice(keys):
     for inp, res in zip(inputs, expected_results):
         choice = map_key_to_choice(*inp)
         assert choice == res
+
+
+def test_save_dict(tmpdir):
+    """Test saving dict to csv."""
+    savedict = dict(a=55, b=22, c=33)
+    fname = tmpdir / "try.csv"
+    save_dict(fname, savedict)
+    savedict.update(a=44, b=33, c=22)
+    save_dict(fname, savedict)
+    df = pd.read_csv(fname, sep="\t")
+    assert df.columns.to_list() == ["a", "b", "c"]
+    np.testing.assert_array_equal(df["a"].to_numpy(), [55, 44])
+    np.testing.assert_array_equal(df["b"].to_numpy(), [22, 33])
+    np.testing.assert_array_equal(df["c"].to_numpy(), [33, 22])
